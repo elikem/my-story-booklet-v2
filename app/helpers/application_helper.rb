996af9705e78b8f8606373865ee177c7a766608d
Elikem Adadevoh <elikem@gmail.com>
2020-03-22 21:58:04 -0400

    Add Devise - Forgot Password

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,11 +11,27 @@ module ApplicationHelper
     User.find_by_id(user_id).stories.count > 0 ? true : false
   end
 
+  # the top navigation uses this to determine whether to show links for a logged in user or for an unauthenticated
+  # user.
   def show_link_to_sign_in_page_or_logged_in_user_links
     if user_signed_in?
       render(partial: "shared/top-navigation/logged_in_user_links")
     else
-      render(partial: "shared/top-navigation/link_sign_in_page")
+      render(partial: "shared/top-navigation/link_to_sign_in_page")
+    end
+  end
+
+  # https://coderwall.com/p/jzofog/ruby-on-rails-flash-messages-with-bootstrap
+  # provides bootstrap classes to flash messages
+  def flash_class(level)
+    # level might come in as a string or symbol
+    level = level.to_sym
+
+    case level
+      when :notice then "alert alert-info"
+      when :alert then "alert alert-warning"
+      when :success then "alert alert-success"
+      when :error then "alert alert-error"
     end
   end
 end
