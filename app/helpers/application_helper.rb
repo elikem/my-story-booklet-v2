50ee14410dbb0bd33e66e32cfdf671841abc4727
Elikem Adadevoh <elikem@gmail.com>
2020-03-23 00:43:06 -0400

    Add Devise - Change forgotten password

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,9 +29,9 @@ module ApplicationHelper
 
     case level
       when :notice then "alert alert-info"
-      when :alert then "alert alert-warning"
       when :success then "alert alert-success"
-      when :error then "alert alert-error"
+      when :alert then "alert alert-danger"
+      when :error then "alert alert-danger"
     end
   end
 end
