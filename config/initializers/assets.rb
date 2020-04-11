65fcfa246e1d14ab81a646a0d30849b9af0764ba
Elikem Adadevoh <elikem@gmail.com>
2020-04-10 22:40:47 -0400

    Fix missing stylesheets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,8 +17,14 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( devise.js )
 Rails.application.config.assets.precompile += %w( pages.css )
 Rails.application.config.assets.precompile += %w( dashboard.css )
-Rails.application.config.assets.precompile += %w( devise/sessions.css )
 Rails.application.config.assets.precompile += %w( stories.css )
+Rails.application.config.assets.precompile += %w( devise/sessions.css )
+Rails.application.config.assets.precompile += %w( devise/registrations.css )
+Rails.application.config.assets.precompile += %w( devise/passwords.css )
+Rails.application.config.assets.precompile += %w( devise/confirmations.css )
+Rails.application.config.assets.precompile += %w( devise/mailer.css )
+Rails.application.config.assets.precompile += %w( devise/registrations.css )
+Rails.application.config.assets.precompile += %w( devise/unlocks.css )
 
 # https://makandracards.com/makandra/29567-managing-vendor-libraries-with-the-rails-asset-pipeline
 Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/asset-libs/*"].sort_by { |dir| -dir.size }
