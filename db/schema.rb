04c594377908ae94b3c001e1de82878b72632fa6
Elikem Adadevoh <elikem@gmail.com>
2020-04-16 20:28:53 -0400

    Add story title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_233607) do
+ActiveRecord::Schema.define(version: 2020_04_16_044014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_01_15_233607) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_stories_on_user_id"
   end
 
