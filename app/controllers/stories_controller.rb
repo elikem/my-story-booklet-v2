ec5ea7f88ae0f30d4189306498003a82ec084a96
Elikem Adadevoh <elikem@gmail.com>
2020-04-20 03:19:54 -0400

    Add version number when story is published

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -46,10 +46,26 @@ class StoriesController < ApplicationController
     redirect_to controller: "dashboard", action: "show"
   end
 
+  def publish
+    @story = Story.find(params[:id])
+
+    if @story.update_attribute(:version_number, @story.version_number.to_i + 1)
+      render "publish"
+    else
+      render "publish"
+    end
+
+    # create version numbers
+    # create user folder with IDML files
+    # output xml files w/ format email-address_version-number_filename.xml
+    # replace xml files
+    # generate IDML file w/ format email-address_version-number_mystorybooklet.idml
+  end
+
   private
 
   def story_params
-    params.require(:story).permit(:content, :language, :status, :user_id, :title)
+    params.require(:story).permit(:content, :language, :status, :user_id, :title, :version_number)
   end
 
   # Ensures that the user story being saved belongs to the authenticated user. If not, redirect to new_story_path.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :stories#, except: :index
+  resources :stories do
+    member do
+      get "publish"
+    end
+  end
 
   devise_for :users
 
