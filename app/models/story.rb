6cb37e55243a551a7f5fb1a45f5d0e9d48b5e0a0
Elikem Adadevoh <elikem@gmail.com>
2020-05-28 18:44:14 -0400

    Annotate model and routes

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: stories
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  language   :string
+#  status     :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_stories_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Story < ApplicationRecord
   require "erb"
   require "fileutils"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,39 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  country_of_residence   :string
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  failed_attempts        :integer          default(0), not null
+#  first_name             :string
+#  languages_spoken       :text             default([]), is an Array
+#  last_name              :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
+#  locked_at              :datetime
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  unconfirmed_email      :string
+#  unlock_token           :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_unlock_token          (unlock_token) UNIQUE
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :timeoutable
