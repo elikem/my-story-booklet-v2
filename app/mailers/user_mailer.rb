aebc7f569b4f9ed4c66ea0fab497215b91b05983
Eli Adadevoh <elikem@gmail.com>
2020-07-22 08:58:57 -0400

    Send PDF to participant

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,8 +5,11 @@ class UserMailer < ApplicationMailer
         @publication = Publication.find(publication_id)
         @user = @publication.story.user
         # replace the extension .idml with .pdf when name the attachment stored in the database
-        @publication_filename = @publication.publication_filename.gsub(".idml", ".pdf")
+        @publication_filename = @publication.pdf_publication_filename
         
+        attachments[@publication.pdf_publication_filename] = File.read("#{@publication.publication_folder_path}/#{@publication.pdf_publication_filename}")
         mail(to: @user.email, subject: "My Story Booklet: Your Story is Ready!")
+
+        # TODO: Fix email template with the correct messaging (both erb and txt)
     end
 end
