e95361d9acf1fc634196b9c1e90d77dedda7951a
Eli Adadevoh <elikem@gmail.com>
2021-06-03 00:18:18 -0400

    remove public profile checkbox and enable public page by default

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,23 +1,8 @@
 class ProfilesController < ApplicationController
   def show
-    flash[:alert] = "User profile not found"; redirect_to controller: "errors", action: "show" unless @profile = Profile.friendly.find(params[:id])
-    flash[:alert] = "User not found"; redirect_to controller: "errors", action: "show" unless @user = Profile.find(@profile.id).user
-    flash[:alert] = "Publication not found"; redirect_to controller: "errors", action: "show" unless @publication = Publication.get_latest_publication_with_pdf(params[:id])
-
-    flash[:alert] = "This profile is not public"; redirect_to controller: "errors", action: "show" unless @profile.public_access
-  end
-
-  def update
-    @profile = Profile.find_by_username(params[:id])
-    @user = @profile.user
-
-    respond_to do |format|
-      if @profile.update_attributes(profile_params)
-        format.js
-      else
-        format.js
-      end
-    end
+    @profile = Profile.friendly.find(params[:id])
+    @user = Profile.find(@profile.id).user
+    @publication = Publication.get_latest_publication_with_pdf(params[:id])
   end
 
   def pdf
@@ -27,9 +12,4 @@ class ProfilesController < ApplicationController
     send_file("#{@latest_publication.pdf_file_path}", type: "application/pdf", disposition: "attachment", stream: true, status: 200, filename: "#{@user.first_name.downcase}-#{@user.last_name.downcase}-mystorybooklet-#{@latest_publication.publication_number}.pdf")
   end
 
-  private
-
-  def profile_params
-    params.require(:profile).permit(:public_access)
-  end
 end
